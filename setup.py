229c0ab1ba00d5037bb11c5d32919ee6fee79dcd
Alex Sadleir <alex.sadleir@linkdigital.com.au>
2013-09-03 11:06:17 +1000

    initial commit

--- setup.py
+++ setup.py
@@ -1,34 +1,26 @@
 from setuptools import setup, find_packages
 
-version = '0.2'
+version = '0.1'
 
 setup(
-	name='ckanext-example',
+	name='ckanext-datagovau',
 	version=version,
-	description='Example extension for customising CKAN',
+	description='Extension for customising CKAN for data.gov.au',
 	long_description='',
 	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 	keywords='',
-	author='Seb Bacon',
-	author_email='seb.bacon@gmail.com',
+	author='Alex Sadleir',
+	author_email='alex.sadleir@linkdigital.com.au',
 	url='',
 	license='',
 	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.example'],
+	namespace_packages=['ckanext', 'ckanext.datagovau'],
 	include_package_data=True,
 	zip_safe=False,
 	install_requires=[],
 	entry_points=\
 	"""
         [ckan.plugins]
-	    example=ckanext.example.plugin:ExamplePlugin
-        example_datasetform=ckanext.example.forms:ExampleDatasetForm
-        example_groupform=ckanext.example.forms:ExampleGroupForm        
-
-        [ckan.forms]
-        example_form = ckanext.example.package_form:get_example_fieldset
-
-        [paste.paster_command]
-        example=ckanext.example.commands:ExampleCommand
+	datagovau=ckanext.datagovau.plugin:ExampleIDatasetFormPlugin
 	""",
 )
