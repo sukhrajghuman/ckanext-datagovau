bc258c24c3e718bcd5784ac2008e6e466690bfe8
Motornyuk Sergey <tonnydexter@gmail.com>
2015-01-09 19:38:20 +0200

    LDSUPP-414 / some progress. Paused on figure 28

---
+++ ckanext/datagovau/theme/public/js/custom.js
@@ -0,0 +1,54 @@
+window.onload = windowOnLoad()
+
+
+
+function windowOnLoad () {
+  addAltToAvatar();
+  addTextToI();
+  addTitleToSearch();
+  fixPager();
+}
+
+
+
+
+
+function addAltToAvatar () {
+  var name = $('span.username').text();
+  var user = $('img.gravatar');
+  _updateAttribute(user, 'alt', (name || 'User' ) + ' profile image')
+}
+
+function addTitleToSearch () {
+  var search = $('input.search');
+  _updateAttribute(search, 'title', 'Search:')
+  
+  search = $('input#field-sitewide-search');
+  _updateAttribute(search, 'title', 'Sitewidth search:')
+}
+
+function addTextToI () {
+  _addReaderTextToButtons('i.icon-remove', 'Remove item');
+  _addReaderTextToButtons('form.site-search  i.icon-search', 'Search');
+}
+
+function fixPager () {
+  $('div.pagination li.disabled').text('<span>...</span>');
+  var first = $('div.pagination a').first()
+  first.text(a.text().replace('«', 'Next page'))
+  var last = $('div.pagination a').last()
+  last.text(a.text().replace('»', 'Previous page'))
+}
+
+
+// u'«', symbol_next=u'»',
+
+function _updateAttribute (element, attr, value) {
+  element.attr(attr, element.attr(attr) || value)
+}
+
+function _addReaderTextToButtons(selector, text){
+  var self = $(selector);
+  if ( self.next().is('span') ) return;
+  self.after($('<span>').text(text))
+}
