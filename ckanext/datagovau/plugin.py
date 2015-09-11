1592846b2e951aba220ba4cdac2a685d0bccfc3e
Alexander Sadleir <maxious@lambdacomplex.org>
2015-09-11 16:08:35 +1000

    Unpub

--- ckanext/datagovau/plugin.py
+++ ckanext/datagovau/plugin.py
@@ -1,21 +1,16 @@
-import logging
-
 import ckan.plugins as plugins
 import ckan.lib as lib
-import ckan.lib.dictization.model_dictize as model_dictize
 import ckan.plugins.toolkit as tk
 import ckan.model as model
 import ckan.logic as logic
+import ckanext.datastore.db as datastore_db
 import os
-from pylons import config
 
-from sqlalchemy import orm
-import ckan.model
 
 import ckanext.datagovau.action as action
-from ckan.lib.plugins import DefaultGroupForm
 from ckan.lib.plugins import DefaultOrganizationForm
 from ckan.lib import uploader, formatters
+
 # get user created datasets and those they have edited
 def get_user_datasets(user_dict):
     created_datasets_list = user_dict['datasets']
@@ -45,7 +40,10 @@ def get_ddg_site_statistics():
     result = model.Session.execute("select count(*) from package where package.state='active' "
                                    "and package.type ='dataset' and package.private = 'f' ").first()[0]
     stats['dataset_count'] = result
-    stats['group_count'] = len(logic.get_action('group_list')({}, {}))
+    result = model.Session.execute("select count(*) from package where package.state='active' "
+                                   "and package.type ='dataset' and package.private = 'f' and package.id in "
+                                   "(select package_id from package_extra where key = 'unpublished' and value='True') ").first()[0]
+    stats['unpub_data_count'] = result
     stats['organization_count'] = len(
         logic.get_action('organization_list')({}, {}))
     result = model.Session.execute(
@@ -56,11 +54,18 @@ def get_ddg_site_statistics():
     result = model.Session.execute(
         '''select count(*) from resource
         where resource.state='active' and        
-        (webstore_url = 'active' or format='wms')
+        (format='wms')
         and package_id not IN
         (select distinct package_id from package INNER JOIN package_extra
         on package.id = package_extra.package_id where key = 'harvest_portal')
         ''').first()[0]
+    result = result + len(datastore_db.get_all_resources_ids_in_datastore())
+    #resources_sql = sqlalchemy.text(u'''SELECT name FROM "_table_metadata"
+    #                                    WHERE alias_of IS NULL''')
+    #extras = {'url': config.get('ckan.datastore.read_url')}
+    #query = sqlalchemy.engine_from_config(config,
+    #                                      'ckan.datastore.sqlalchemy.',
+    #                                      **extras).execute(resources_sql)
     stats['api_count'] = result
 
     return stats
